32dd24332f06bc0c50fb32355d4e4d38c945bb57
Unknown <mild.mild987@gmail.com>
2017-11-24 13:59:55 +0700

    ข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout <<"Miss." <<endl << "Sasawan Nopphakat"<<endl <<"mild"<<endl <<"Sex Femal"<<endl <<"03082541"<<endl <<"PlayGame"<<endl <<"sigle"<<endl;
   return 0;
 }
